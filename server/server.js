ed526bf6cd6f3e581952dd35abb73921b2beaa23
rlobz <rlobzowski@gmail.com>
2024-03-07 15:03:08 +0000

    Create an Apollo Provider to make every request work with the Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
-const db = require('./config/connection');
-
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
@@ -18,15 +17,16 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/"));
 });
 
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
